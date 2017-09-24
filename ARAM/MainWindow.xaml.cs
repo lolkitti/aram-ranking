ea3078006cb1f305b9ca4a46be41c609992240c2
Jun <32242005+lolkitti@users.noreply.github.com>
2017-09-24 17:48:34 +0200

    Change to C#

---
+++ ARAM/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RiotWrapper.Summoner_V3;
+
+namespace ARAM
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            var sum = new Summoner();
+            string apikey = "RGAPI-6369a470-4345-458e-9a0d-1d9a036af1c2";
+        }
+
+    }
+}
--- Aram-Ranked/MainWindow.xaml.vb
+++ Aram-Ranked/MainWindow.xaml.vb
@@ -1,3 +1,15 @@
-﻿Class MainWindow
+﻿Imports RiotWrapper.Summoner_V3
 
+Class MainWindow
+    Dim summonername As String
+    Dim level As Integer
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Dim API As New Summoner
+        API.SummonerLevel
+    End Sub
+
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+
+    End Sub
 End Class
